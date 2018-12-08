c8bb2fd57ba58247d81b871747611d3fd3426235
Benjamin Olayinka <ben@Benjamins-MacBook-Pro.local>
2018-12-08 13:08:21 +0100

    ben eq

--- js/app.js
+++ js/app.js
@@ -370,3 +370,41 @@ function cleardb2() {
 function __log(e, data) {
 	log.innerHTML += "\n" + e + " " + (data || '');
 }
+
+function knobs() {
+	var knob = pureknob.createKnob(300, 300);
+				
+	// Set properties.
+	knob.setProperty('angleStart', -0.75 * Math.PI);
+	knob.setProperty('angleEnd', 0.75 * Math.PI);
+	knob.setProperty('colorFG', '#88ff88');
+	knob.setProperty('trackWidth', 0.4);
+	knob.setProperty('valMin', 0);
+	knob.setProperty('valMax', 100);
+	
+	// Set initial value.
+	knob.setValue(50);
+	
+	/*
+	 * Event listener.
+	 *
+	 * Parameter 'knob' is the knob object which was
+	 * actuated. Allows you to associate data with
+	 * it to discern which of your knobs was actuated.
+	 *
+	 * Parameter 'value' is the value which was set
+	 * by the user.
+	 */
+	var listener = function(knob, value) {
+		console.log(value);
+	};
+	
+	knob.addListener(listener);
+	
+	// Create element node.
+	var node = knob.node();
+	
+	// Add it to the DOM.
+	var elem = document.getElementById('knob1');
+	elem.appendChild(node);
+}
