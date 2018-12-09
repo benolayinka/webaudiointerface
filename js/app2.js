c30f8a95836ec43a669642175ca7af06e5a1c950
Benjamin Olayinka <ben@Benjamins-MacBook-Pro.local>
2018-12-09 16:08:46 +0100

    record

--- js/app2.js
+++ js/app2.js
@@ -1,44 +1,26 @@
-var selectors;
+var recorder; 						//WebAudioRecorder object
+var input; 							//MediaStreamAudioSourceNode  we'll be recording
+var encodingType; 					//holds selected encoding for resulting audio (file)
+var encodeAfterRecord = true;       // when to encode
 
 window.onload = function() {
 
-	var e = document.createElement("div");
+	var encodingTypeSelect = document.getElementById("encodingTypeSelect");
+	var recordButton = document.getElementById("recordButton");
+	var stopButton = document.getElementById("stopButton");
 
-	var track1 = new Track("track1", e);
+	//add events to those 2 buttons
+	recordButton.addEventListener("click", startRecording);
+	stopButton.addEventListener("click", stopRecording);
+
+	var track1 = new Track("track1", document.getElementById("track1"));
+	var track2 = new Track("track2", document.getElementById("track2"));
 
 	navigator.requestMIDIAccess()
 	    .then(onMIDISuccess, onMIDIFailure);
 
 	track1.start();
 
-}
-
-function gotDevices(deviceInfos) {
-  // Handles being called several times to update labels. Preserve values.
-  const values = selectors.map(select => select.value);
-  selectors.forEach(select => {
-    while (select.firstChild) {
-      select.removeChild(select.firstChild);
-    }
-  });
-  for(item in selectors){
-	  for (let i = 0; i !== deviceInfos.length; ++i) {
-	    const deviceInfo = deviceInfos[i];
-	    const option = document.createElement('option');
-	    option.value = deviceInfo.deviceId;
-	    if (deviceInfo.kind === 'audioinput') {
-	      option.text = deviceInfo.label || `microphone ${selectors[item].length + 1}`;
-	      selectors[item].appendChild(option);
-	    } else {
-	      console.log('Some other kind of source/device: ', deviceInfo);
-	    }
-	}
-  }
-  selectors.forEach((select, selectorIndex) => {
-    if (Array.prototype.slice.call(select.childNodes).some(n => n.value === values[selectorIndex])) {
-      select.value = values[selectorIndex];
-    }
-  });
 }
 
 function onMIDISuccess(midiAccess) {
@@ -57,4 +39,102 @@ function getMIDIMessage(midiMessage) {
 
 function onMIDIFailure() {
     console.log('Could not access your MIDI devices.');
+}
+
+function startRecording() {
+	console.log("startRecording() called");
+
+		//update the format 
+		document.getElementById("formats").innerHTML="Format: 2 channel "+encodingTypeSelect.options[encodingTypeSelect.selectedIndex].value+" @ 44.1 kHz"
+
+		//get the encoding 
+		encodingType = encodingTypeSelect.options[encodingTypeSelect.selectedIndex].value;
+		
+		//disable the encoding selector
+		encodingTypeSelect.disabled = true;
+
+		//ben recording output of audio context
+		recorder = new WebAudioRecorder(window.outputtrack, {
+		  workerDir: "js/", // must end with slash
+		  encoding: encodingType,
+		  numChannels:2, //2 is the default, mp3 encoding supports only 2
+		  onEncoderLoading: function(recorder, encoding) {
+		    // show "loading encoder..." display
+		    __log("Loading "+encoding+" encoder...");
+		  },
+		  onEncoderLoaded: function(recorder, encoding) {
+		    // hide "loading encoder..." display
+		    __log(encoding+" encoder loaded");
+		  }
+		});
+
+		recorder.onComplete = function(recorder, blob) { 
+			__log("Encoding complete");
+			createDownloadLink(blob,recorder.encoding);
+			encodingTypeSelect.disabled = false;
+		}
+
+		recorder.setOptions({
+		  timeLimit:120,
+		  encodeAfterRecord:encodeAfterRecord,
+	      ogg: {quality: 0.5},
+	      mp3: {bitRate: 160}
+	    });
+
+		//start the recording process
+		recorder.startRecording();
+
+		 __log("Recording started");
+
+	//disable the record button
+    recordButton.disabled = true;
+    stopButton.disabled = false;
+}
+
+function stopRecording() {
+	console.log("stopRecording() called");
+	
+	//stop microphone access
+	gumStream.getAudioTracks()[0].stop();
+
+	//ben
+	audioPlaying = false;
+
+	//disable the stop button
+	stopButton.disabled = true;
+	recordButton.disabled = false;
+	
+	//tell the recorder to finish the recording (stop recording + encode the recorded audio)
+	recorder.finishRecording();
+
+	__log('Recording stopped');
+}
+
+function createDownloadLink(blob,encoding) {
+	
+	var url = URL.createObjectURL(blob);
+	var au = document.createElement('audio');
+	var li = document.createElement('li');
+	var link = document.createElement('a');
+
+	//add controls to the <audio> element
+	au.controls = true;
+	au.src = url;
+
+	//link the a element to the blob
+	link.href = url;
+	link.download = new Date().toISOString() + '.'+encoding;
+	link.innerHTML = link.download;
+
+	//add the new audio and a elements to the li element
+	li.appendChild(au);
+	li.appendChild(link);
+
+	//add the li element to the ordered list
+	recordingsList.appendChild(li);
+}
+
+//helper function
+function __log(e, data) {
+	log.innerHTML += "\n" + e + " " + (data || '');
 }
